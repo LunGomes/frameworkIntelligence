303703c6def58267da6a92618eb481486d38b4e0
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-26 11:54:22 -0300

    Exercicíos de Comparação

---
+++ Aula2603/js/script.js
@@ -0,0 +1,98 @@
+let A = 10;
+console.log(A);
+let B = 20;
+console.log(B);
+let C = B/A;
+console.log(C);
+let D = C*A;
+console.log(D);
+let J = D+B+A;
+console.log(J);
+console.log('---------------------');
+
+A = 30;
+console.log(A);
+B = -20;
+console.log(B);
+C = A - B;
+console.log(C);
+C = C + 10;
+console.log(C);
+let K = C * 2;
+console.log(K);
+console.log('---------------------');
+
+A = 12; 
+console.log(A);
+B = A * 12;
+console.log(B);
+C = A * 12;
+console.log(C);
+B = C * 0;
+console.log(B);
+let L = B + 12;
+console.log(L);
+console.log('---------------------');
+
+let P = 1000;
+console.log(P);
+let Q = 100;
+console.log(Q);
+let R = 10;
+console.log(R);
+let S = 1;
+console.log(S);
+let M = (P + Q + R + S) / Q;
+console.log(M);
+console.log('---------------------');
+
+A = 10;
+console.log(A);
+B = A + 10;
+console.log(B);
+C = B + 10;
+console.log(C);
+D = C + 10;
+console.log(D)
+let N = D + 10;
+console.log(N);
+console.log('---------------------');
+
+A = 10;
+B = 9;
+C = 15;
+D = 10;
+console.log(A === D);
+console.log(A < B);
+console.log(B > A);
+console.log(A > C);
+console.log((C > A) || (C === A));
+console.log('---------------------');
+
+console.log(A === B);
+console.log((A === D) && (B > A));
+console.log((A < C) || (A = B));
+console.log((A === D) || (A === C) || (A === B));
+console.log((A < B) && (D < A));
+console.log('---------------------');
+
+console.log(A === C);
+console.log((B <= A) || (D > B));
+console.log((A == A) && (D === B));
+console.log((A + B) < (C + D));
+console.log((A + B + C + D) > 0);
+console.log('---------------------');
+
+A = 8;
+B = 2;
+C = 6;
+D = 1;
+let resultado = ((A ** B) + (C % B)) - ((D * 2) + (D * 3) + (A * B % 4));
+console.log(resultado);
+console.log(resultado === 87);
+
+resultado = (A + (C % 2)) - ((B * A) * 3) + D; 
+console.log(resultado);
+
+
+
